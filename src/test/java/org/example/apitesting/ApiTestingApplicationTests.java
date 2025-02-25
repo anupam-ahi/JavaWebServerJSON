0348452de9dcb840f4d9573a3ee82ff372fe1969
Anupam Ahi <anupam.gollapalli@gmail.com>
2025-02-25 08:45:35 -0800

    Added the files to git repo

---
+++ src/test/java/org/example/apitesting/ApiTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.apitesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTestingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
